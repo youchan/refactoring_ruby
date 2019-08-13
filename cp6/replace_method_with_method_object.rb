b9acc422a87321c9a0d8c7be9baf03b88346a883
ken3ypa <kirie115@gmail.com>
2019-08-14 08:56:40 +0900

    クラスとコンストラクタの追加

--- cp6/replace_method_with_method_object.rb
+++ cp6/replace_method_with_method_object.rb
@@ -15,3 +15,14 @@ class Account
     end
 end
 
+class Gamma
+  attr_reader :account, :input_val, :quantity, :year_to_date,
+              :important_value1, :important_value2, :important_value3
+
+  def initialize(account, input_val_arg, quantity_arg, year_to_date_arg)
+    @account = account
+    @input_val = input_val_arg
+    @quantity = quantity_arg
+    @year_to_date = year_to_date_arg
+  end
+end
