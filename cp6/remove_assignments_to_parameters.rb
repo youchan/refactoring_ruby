976ab2c777c005f567c81375e280cc85103511d9
ken3ypa <kirie115@gmail.com>
2019-08-13 21:19:20 +0900

    引数への代入の除去 EXAMPLE

--- cp6/remove_assignments_to_parameters.rb
+++ cp6/remove_assignments_to_parameters.rb
@@ -1,6 +1,6 @@
 def discount(input_val, quantity, year_to_date)
-  result = input_val
-  if input_val > 50
-    result -= 2
-  end
+  input_val -= 2 if input_val > 50
+  input_val -= 1 if quantity > 100
+  input_val -= 4 if year_to_date > 10000
+  input_val
 end
