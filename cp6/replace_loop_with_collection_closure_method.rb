77fb24a5041c1922020defb195dad1949b8f6988
ken3ypa <kirie115@gmail.com>
2019-08-14 08:38:22 +0900

    selectで書き換え

--- cp6/replace_loop_with_collection_closure_method.rb
+++ cp6/replace_loop_with_collection_closure_method.rb
@@ -1,6 +1,3 @@
 # 6.11 ループからコレクションクロージャメソッドへ（Replace Loop with Collection Closure Method）
 
-managers = []
-employees.each do |e|
-  managers << e if e.manager?
-end
+managers = employees.select {|e| e.manager?}
