e4d620e8b4ccab9920a2714a877dc27dfa9031f9
ken3ypa <kirie115@gmail.com>
2019-08-15 08:52:20 +0900

    オプション引数だけに名前をつける

---
+++ cp6/introduce_named_parameter_example_2nd.rb
@@ -0,0 +1,15 @@
+# SQL構築コード
+
+class Books
+  def self.find(selector, conditions = '', *joins)
+    sql = ['SELECT * FROM books']
+    joins.each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table} ON"
+      sql << "books.#{join_table.to_s.chop}_id"
+      sql << " = #{join_table}.id"
+    end
+    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << 'LIMIT 1' if selector == :first
+    connection.find(sql.join(' '))
+  end
+end
