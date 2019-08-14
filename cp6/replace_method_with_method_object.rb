3ce69dafdc2e5487e4ed59a70ad1d70e64c16df5
ken3ypa <kirie115@gmail.com>
2019-08-14 09:13:57 +0900

    important_thingメソッドの切り出し
    その他タイポミス修正、deltaメソッドの実装

--- cp6/replace_method_with_method_object.rb
+++ cp6/replace_method_with_method_object.rb
@@ -4,6 +4,10 @@ class Account
   def gamma(input_val, quantity, year_to_date)
     Gamma.new(self, input_val, quantity, year_to_date).compute
   end
+
+  def delta
+    1
+  end
 end
 
 class Gamma
@@ -18,15 +22,19 @@ class Gamma
   end
 
   def compute
-    inportant_value1 = (input_val * quantity) + @account.delta
-    important_value2 = (input_val * year_to_date) + 100
+    @important_value1 = (input_val * quantity) + @account.delta
+    @important_value2 = (input_val * year_to_date) + 100
 
-    if (year_to_date - important_value1) > 100
-      important_value2 -= 20
-    end
+    important_thing
 
-    important_value3 = important_value2 * 7
+    @important_value3 = important_value2 * 7
     # and so on.
-    important_value3 - 2 * important_value1
+    @important_value3 - 2 * important_value1
+  end
+
+  def important_thing
+    return @important_value2 -= 20 if (year_to_date - important_value1) > 100
   end
 end
+
+# p Account.new.gamma(1, 1, 2)
