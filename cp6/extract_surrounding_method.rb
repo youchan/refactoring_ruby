0b44576e2127ebc10564763c4a0844e65c087d94
ken3ypa <kirie115@gmail.com>
2019-08-14 09:49:17 +0900

    サンドイッチメソッドの抽出 EXAMPLE

--- cp6/extract_surrounding_method.rb
+++ cp6/extract_surrounding_method.rb
@@ -1,19 +1,34 @@
 # 6.12 サンドイッチメソッドの抽出（Extract Surrounding Method）
 
-def charge(amount, credit_card_number)
-  connect do |connection|
-    connection.send(amount, credit_card_number)
-  end
-end
+class Person
+  attr_reader :mother, :children, :name
 
-def connect
-  begin
-    connection = CreditCardServer.connect(...)
-    yield connection
-  rescue IOError => e
-    Logger.log "Could not submit order #{@order_number} to the server: #{e}"
-    return nil
-  ensure
-    connection.close
+  def initialize(name, date_of_birth, date_of_death = nil, mother = nil)
+    @name, @mother = name, mother
+    @date_of_birth, @date_of_death = date_of_birth, date_of_death
+    @children = []
+    @mother.add_child(self) if @mother
+  end
+
+  def add_child(child)
+    @children << child
+  end
+
+  def number_of_living_descendants
+    children.inject(0) do |count, child|
+      count += 1 if child.alive?
+      count + child.number_of_living_descendants
+    end
+  end
+
+  def number_of_descendants_named(name)
+    children.inject(0) do |count, child|
+      count += 1 if child.name == name
+      count + child.number_of_descendants_named(name)
+    end
+  end
+
+  def alive?
+    @date_of_death.nil?
   end
 end
