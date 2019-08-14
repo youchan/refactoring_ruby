354d7c81ca7b7de6a472ed4733a67456dd67cd94
ken3ypa <kirie115@gmail.com>
2019-08-14 09:45:35 +0900

    connectメソッドに抽出してyieldで呼び出し

--- cp6/extract_surrounding_method.rb
+++ cp6/extract_surrounding_method.rb
@@ -1,9 +1,15 @@
 # 6.12 サンドイッチメソッドの抽出（Extract Surrounding Method）
 
 def charge(amount, credit_card_number)
+  connect do |connection|
+    connection.send(amount, credit_card_number)
+  end
+end
+
+def connect
   begin
     connection = CreditCardServer.connect(...)
-    connection.send(amount, credit_card_number)
+    yield connection
   rescue IOError => e
     Logger.log "Could not submit order #{@order_number} to the server: #{e}"
     return nil
