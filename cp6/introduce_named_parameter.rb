c36aee62c4acf2f3129e78ba2be84eb8a7dea1c0
ken3ypa <kirie115@gmail.com>
2019-08-14 21:06:27 +0900

    名前付き引数の導入 EXAMPLE

--- cp6/introduce_named_parameter.rb
+++ cp6/introduce_named_parameter.rb
@@ -1,3 +1,13 @@
 # 6.14 名前付き引数の導入（Introduce Named Parameter）
 
-SearchCriteria.new(:author_id => 5, :publisher_id => 8, :isbn =>"0201485672")
+class SearchCriteria
+  attr_reader :author_id, :publisher_id, :isbn
+
+  def initialize(author_id, publisher_id, isbn)
+    @author_id = author_id
+    @publisher_id = publisher_id
+    @isbn = isbn
+  end
+end
+
+criteria = SearchCriteria.new(5, 8, "0201485672")
