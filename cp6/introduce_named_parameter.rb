3319ea6fb899237811652e30e911014fb26dbda3
ken3ypa <kirie115@gmail.com>
2019-08-15 08:36:40 +0900

    hash_initializerメソッドを使って動的にinitializeメソッドとインスタンス変数を定義

--- cp6/introduce_named_parameter.rb
+++ cp6/introduce_named_parameter.rb
@@ -1,13 +1,21 @@
 # 6.14 名前付き引数の導入（Introduce Named Parameter）
 
-class SearchCriteria
-  attr_reader :author_id, :publisher_id, :isbn
-
-  def initialize(params)
-    @author_id    = params[:author_id]
-    @publisher_id = params[:publisher_id]
-    @isbn         = params[:isbn]
+module CustomInitializers
+  def hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
   end
+end
+
+Class.send :include, CustomInitializers
+
+
+class SearchCriteria
+  hash_initializer :author_id, :publisher_id, :isbn
 end
 
 criteria = SearchCriteria.new(author_id: 5, publisher_id: 8, isbn: "0201485672")
