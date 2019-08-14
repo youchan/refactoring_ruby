d561329dcf52d2e87e2172e0403435246ad0d51e
ken3ypa <kirie115@gmail.com>
2019-08-14 09:40:20 +0900

    6.10 アルゴリズム変更

--- cp6/substitute_algorithm.rb
+++ cp6/substitute_algorithm.rb
@@ -1,1 +1,19 @@
 # 6.10 アルゴリズム変更（Substitute Algorithm）
+#
+def found_friends(people)
+  friends = []
+  people.each do |person|
+    if (person == 'Don')
+      friends << 'Don'
+    end
+
+    if (person == 'John')
+      friends << 'John'
+    end
+
+    if (person == 'Kent')
+      friends << 'Kent'
+      end
+    end
+  friends
+end
