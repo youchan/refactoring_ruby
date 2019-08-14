875838c971b4be109ebb96ca554b6d7b3889d65e
ken3ypa <kirie115@gmail.com>
2019-08-14 09:41:50 +0900

    新しいアルゴリズムで置き換え

--- cp6/substitute_algorithm.rb
+++ cp6/substitute_algorithm.rb
@@ -1,19 +1,5 @@
 # 6.10 アルゴリズム変更（Substitute Algorithm）
 #
 def found_friends(people)
-  friends = []
-  people.each do |person|
-    if (person == 'Don')
-      friends << 'Don'
-    end
-
-    if (person == 'John')
-      friends << 'John'
-    end
-
-    if (person == 'Kent')
-      friends << 'Kent'
-      end
-    end
-  friends
+  people.select { |person| %w[Don John Kent].include? person }
 end
