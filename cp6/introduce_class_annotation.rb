6bdd2df7eb922eabac20112306a48c89d2ad0de4
ken3ypa <kirie115@gmail.com>
2019-08-15 09:18:32 +0900

    hash_initializerメソッドの追加

--- cp6/introduce_class_annotation.rb
+++ cp6/introduce_class_annotation.rb
@@ -1,7 +1,14 @@
 # 6.13 クラスアノテーションの導入（Introduce Class Annotation）
 
 class SearchCriteria
+  def self.hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "@#{attribute_name}", data[attribute_name]
+      end
+    end
+  end
+
   hash_initializer :author_id, :publisher_id, :isbn
 end
-
-# ↑こう変えられると嬉しい
