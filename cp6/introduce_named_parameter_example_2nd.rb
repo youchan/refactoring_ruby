e461050f6fd23a24e4a4e1f094f007854ebd7682
ken3ypa <kirie115@gmail.com>
2019-08-15 09:00:49 +0900

    condition, joins引数をHashに取り替え、あわせてメソッド定義を書き換え

--- cp6/introduce_named_parameter_example_2nd.rb
+++ cp6/introduce_named_parameter_example_2nd.rb
@@ -1,14 +1,20 @@
 # SQL構築コード
+# NOTE: assert_valid_keys は active_support のメソッドのためrequireしている
+require 'active_support/core_ext/hash/keys.rb'
 
 class Books
-  def self.find(selector, conditions = '', *joins)
+  def self.find(selector, hash = {})
+    hash.assert_valid_keys :conditions, :joins
+    hash[:joins] ||= []
+    hash[:conditions] ||= ''
+
     sql = ['SELECT * FROM books']
-    joins.each do |join_table|
+    hash[:joins].each do |join_table|
       sql << "LEFT OUTER JOIN #{join_table} ON"
       sql << "books.#{join_table.to_s.chop}_id"
       sql << " = #{join_table}.id"
     end
-    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << "WHERE #{ hash[:conditions] }" unless hash[:conditions].empty?
     sql << 'LIMIT 1' if selector == :first
 
     connection.find(sql.join(' '))
