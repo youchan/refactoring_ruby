62846bf3aac50321b0361003170885da9905ebcf
ken3ypa <kirie115@gmail.com>
2019-08-15 09:06:56 +0900

    アサーションの導入

--- cp6/introduce_named_parameter_example_2nd.rb
+++ cp6/introduce_named_parameter_example_2nd.rb
@@ -1,6 +1,18 @@
 # SQL構築コード
-# NOTE: assert_valid_keys は active_support のメソッドのためrequireしている
-require 'active_support/core_ext/hash/keys.rb'
+# NOTE: assert_valid_keys は active_support のメソッドとして実装されているため、requireしている。
+# https://github.com/rails/rails/blob/0b4cfa2ba3e978386240e0e56a409616cc32fd02/activesupport/lib/active_support/core_ext/hash/keys.rb#L77
+# require 'active_support/core_ext/hash/keys.rb'
+
+# EXAMPLE: 自前で書くとこんな感じ
+#
+# module AssertValidKeys
+#   def assert_valid_keys(*valid_keys)
+#     unknown_keys = keys - [valid_keys].flatten
+#     raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") if unknown_keys.any?
+#   end
+# end
+# Hash.send(:include, AssertValidKeys)
+
 
 class Books
   def self.find(selector, hash = {})
