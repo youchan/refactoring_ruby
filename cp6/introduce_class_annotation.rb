00cd57c84c73783ed1c21e740eee25ce062a3e79
ken3ypa <kirie115@gmail.com>
2019-08-15 09:16:21 +0900

    6.13 クラスアノテーションの導入

--- cp6/introduce_class_annotation.rb
+++ cp6/introduce_class_annotation.rb
@@ -1,1 +1,9 @@
 # 6.13 クラスアノテーションの導入（Introduce Class Annotation）
+
+class SearchCriteria
+  def initialize(hash)
+    @author_id = hash[:author_id]
+    @publisher_id = hash[:publisher_id]
+    @isbn = hash[:isbn]
+  end
+end
