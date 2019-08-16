bd1696dddb7be1801ea27b2733fb9d1518df6152
ken3ypa <kirie115@gmail.com>
2019-08-17 08:55:45 +0900

    evalを実行時からパース時へ

--- cp6/dynamic_method_definition.rb
+++ cp6/dynamic_method_definition.rb
@@ -1,1 +1,9 @@
 # 6.17 動的メソッド定義（Dynamic Method Definition）
+
+def failure
+  self.state = :failure
+end
+
+def error
+  self.state = :error
+end
--- cp6/move_eval_from_runtime_to_parse_time.rb
+++ cp6/move_eval_from_runtime_to_parse_time.rb
@@ -1,2 +1,13 @@
 # 6.20 evalを実行時からパース時へ（Move Eval from Runtime to Parse Time）
 
+class Person
+  def self.attr_with_default(options)
+    options.each_pair do |attribute, default_value|
+      define_method attribute do
+        eval "@#{attribute} ||= #{default_value}"
+      end
+    end
+  end
+
+  attr_with_default emails: '[]', employee_number: 'EmployeeNumberGenerator.next'
+end
