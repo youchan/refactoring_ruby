a3ded8b413885fb49d033297cb053eb9cd9e3154
ken3ypa <kirie115@gmail.com>
2019-08-17 11:01:32 +0900

    名前付き引数の除去 EXAMPLE

--- cp6/remove_named_parameter.rb
+++ cp6/remove_named_parameter.rb
@@ -1,3 +1,25 @@
 # 6.15 名前付き引数の除去（Remove Named Parameter）
 
-IsbnSearch.new("0201485672")
+class Books
+  def self.find(hash={})
+    hash[:joins] ||= []
+    hash[:conditions] ||= ""
+    sql = ["SELECT * FROM books"]
+    hash[:joins].each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table}"
+      sql << "ON books.#{join_table.to_s.chop}_id = #{join_table}.id"
+    end
+    sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
+    sql << "LIMIT 1" if hash[:selector] == :first
+    connection.find(sql.join(" "))
+  end
+end
+
+
+Books.find
+Books.find(:selector => :all,
+           :conditions => "authors.name = 'Jenny James'",
+           :joins => [:authors])
+Books.find(:selector => :first,
+           :conditions => "authors.name = 'JennyJames'",
+           :joins => [:authors])
