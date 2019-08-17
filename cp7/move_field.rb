41ee215b53fe6fa073eb78ccb8344a99b69421dc
ken3ypa <kirie115@gmail.com>
2019-08-17 16:55:27 +0900

    Fowardableを利用して委譲

--- cp7/move_field.rb
+++ cp7/move_field.rb
@@ -1,5 +1,16 @@
 class Account
   # attr_accessor :interest_rate
+  extend Forwardable
+
+  # NOTE
+  # def_delegator(accessor, method, ali = method) -> ()
+  # accessor: 委譲先のオブジェクト
+  # method    委譲先のメソッド
+  # ali       委譲元のメソッド
+  def_delegator :@account_type, :interest_rate, :interest_rate=
+  # Accountのオブジェクトで interest_rate= が呼び出された場合
+  # @account_typeのinterest_rateを呼び出す
+
   def interest_for_amount_days(amount, days)
     @account_type.interest_rate * amount * days / 365
   end
