8b7ae2d2212b782701961513f4afa76eaa6c4e92
ken3ypa <kirie115@gmail.com>
2019-08-17 09:04:20 +0900

    6.19 動的レセプタの分離

--- cp6/isolate_dynamic_receptor.rb
+++ cp6/isolate_dynamic_receptor.rb
@@ -1,1 +1,16 @@
 # 6.19 動的レセプタの分離（Isolate Dynamic Receptor）
+
+class Recorder
+  instance_methods.each do |meth|
+    undef_method meth unless meth =~ /^(__|inspect)/
+  end
+
+  def messages
+    @messages ||= []
+  end
+
+  def method_missing(sym, *args)
+    messages << %w[sym args]
+    self
+  end
+end
