f6ccca681d9c1512ef49c75bd2c0b2cb0a7edf76
ken3ypa <kirie115@gmail.com>
2019-08-17 17:45:43 +0900

    インラインクラス

---
+++ cp7/inline_class.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :name
+
+  def initialize
+    @office_telephone = TelephoneNumber.new
+  end
+
+  def telephone_number
+    @office_telephone.telephone_number
+  end
+
+  def office_telephone
+    @office_telephone
+  end
+end
+
+class TelephoneNumber
+  attr_accessor :area_code, :number
+
+  def telephone_number
+    '(' + area_code + ') ' + number
+  end
+end
