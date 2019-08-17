be4940ebad06e7951f5d041dae6141a5d54a61f4
ken3ypa <kirie115@gmail.com>
2019-08-17 09:17:35 +0900

    動的レセプタから動的メソッド定義へ

--- cp6/replace_dynamic_receptor_with_dynamic_method_definition.rb
+++ cp6/replace_dynamic_receptor_with_dynamic_method_definition.rb
@@ -1,1 +1,11 @@
 # 6.18 動的レセプタから動的メソッド定義へ（Replace Dynamic Receptor with Dynamic Method Definition）
+
+class Decorator
+  def initialize(subject)
+    @subject = subject
+  end
+
+  def method_missing(sym, *args, &block)
+    @subject.send sym, *args, &block
+  end
+end
