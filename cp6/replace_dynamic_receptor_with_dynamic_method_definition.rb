a2f1d7821d809dc528865eb734023ebb86f49932
ken3ypa <kirie115@gmail.com>
2019-08-17 09:21:09 +0900

    属性があるかnilかmethod_missingで判定

--- cp6/replace_dynamic_receptor_with_dynamic_method_definition.rb
+++ cp6/replace_dynamic_receptor_with_dynamic_method_definition.rb
@@ -1,13 +1,13 @@
 # 6.18 動的レセプタから動的メソッド定義へ（Replace Dynamic Receptor with Dynamic Method Definition）
 
-class Decorator
-  def initialize(subject)
-    subject.public_methods(false).each do |meth|
-      (class << self; self; end).class_eval do
-        define_method meth do |*args|
-          subject.send meth, *args
-        end
-      end
-    end
+class Person
+  attr_accessor :name, :age
+
+  def method_missing(sym, *args, &block)
+    empty?(sym.to_s.sub(/^empty_/,"").chomp("?"))
+  end
+
+  def empty?(sym)
+    self.send(sym).nil?
   end
 end
