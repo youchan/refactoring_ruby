e201d1ff695dd5015fb720085ecbdd471428140f
ken3ypa <kirie115@gmail.com>
2019-08-17 11:05:01 +0900

    使われていないデフォルト引数の除去

--- cp6/remove_unused_default_parameter.rb
+++ cp6/remove_unused_default_parameter.rb
@@ -1,1 +1,6 @@
 # 6.16 使われていないデフォルト引数の除去（Remove Unused Default Parameter）
+
+def product_count_items(search_criteria=nil)
+  criteria = search_criteria || @search_criteria
+  ProductCountItem.find_all_by_criteria(criteria)
+end
