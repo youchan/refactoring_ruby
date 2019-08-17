5ef563db6d5cd9f5ba490de1526b69ace392144c
ken3ypa <kirie115@gmail.com>
2019-08-17 15:51:13 +0900

    AccountTypeにattr_accessorを追加

--- cp7/move_field.rb
+++ cp7/move_field.rb
@@ -1,5 +1,9 @@
 class Account
   def interest_for_amount_days(amount, days)
-    @interest_rate * amount * days / 365;
+    @account_type.interest_rate * amount * days / 365
   end
+end
+
+class AccountType
+  attr_accessor :interest_rate
 end
