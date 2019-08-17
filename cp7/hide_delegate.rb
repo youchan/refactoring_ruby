a659b28df05d25e64467bf5257a8c0a393c5521f
ken3ypa <kirie115@gmail.com>
2019-08-17 17:54:00 +0900

    委譲の隠蔽

---
+++ cp7/hide_delegate.rb
@@ -0,0 +1,12 @@
+class Person
+  attr_accessor :department
+end
+
+class Department
+  attr_reader :manager
+  def initialize(manager)
+    @manager = manager
+  end
+end
+
+manager = john.department.manager
