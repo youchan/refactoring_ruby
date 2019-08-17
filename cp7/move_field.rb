b1704f49f2b496b6a444f5b588fd44bcdf042ee0
ken3ypa <kirie115@gmail.com>
2019-08-17 16:41:49 +0900

    attr_accessorをメソッド呼び出しに変更

--- cp7/move_field.rb
+++ cp7/move_field.rb
@@ -1,9 +1,10 @@
 class Account
+  # attr_accessor :interest_rate
   def interest_for_amount_days(amount, days)
     @account_type.interest_rate * amount * days / 365
   end
-end
 
-class AccountType
-  attr_accessor :interest_rate
+  def interest_rate
+    @account_type.interest_rate
+  end
 end
