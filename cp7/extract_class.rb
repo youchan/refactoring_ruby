d50f538ceb645a6c9a779298fbcd5b180de3026b
ken3ypa <kirie115@gmail.com>
2019-08-17 17:41:08 +0900

    office_area_codeメソッドを追加しTelephoneNumberクラスに処理を委譲

--- cp7/extract_class.rb
+++ cp7/extract_class.rb
@@ -5,10 +5,19 @@ class Person
   attr_accessor :office_area_code
   attr_accessor :office_number
 
+  def initialize
+    @office_telephone = TelephoneNumber.new
+  end
+
   def telephone_number
-    '(' + @office_area_code + ') ' + @office_number
+    '(' + office_area_code + ') ' + @office_number
+  end
+
+  def office_area_code
+    @office_telephone.area_code
   end
 end
 
 class TelephoneNumber
+  attr_accessor :area_code, :number
 end
