91ca9498393a88d752bb657b50fd929146b3d77b
ken3ypa <kirie115@gmail.com>
2019-08-19 09:13:17 +0900

    orで一文に書き換え

--- cp9/9_3_consolidate_conditional_expression.rb
+++ cp9/9_3_consolidate_conditional_expression.rb
@@ -1,8 +1,6 @@
 # 9.3 条件式の統合（Consolidate Conditional Expression）
 
 def disability_amount
-  return 0 if @seniority < 2
-  return 0 if @months_disabled > 12
-  return 0 if @is_part_time
+  return 0 if @seniority < 2 || @months_disabled > 12 || @is_part_timeend
 end
 # compute the disability amount
