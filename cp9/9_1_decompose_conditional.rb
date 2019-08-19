db40dc2752e4a00f013f141ff5e351ea5cb10b59
ken3ypa <kirie115@gmail.com>
2019-08-19 09:08:15 +0900

    条件をメソッドに切り出し

--- cp9/9_1_decompose_conditional.rb
+++ cp9/9_1_decompose_conditional.rb
@@ -1,7 +1,19 @@
 # 9.1 条件文の分解（Decompose Conditional）
 
-if date < SUMMER_START || date > SUMMER_END
-  charge = quantity * @winter_rate + @winter_service_charge
+if not_summer(date)
+  charge = winter_charge(quantity)
 else
-  charge = quantity * @summer_rate
+  charge = summer_charge(quantity)
+end
+
+def not_summer(date)
+  date < SUMMER_START || date > SUMMER_END
+end
+
+def winter_charge(quantity)
+  quantity * @winter_rate + @winter_service_charge
+end
+
+def summer_charge(quantity)
+  quantity * @summer_rate
 end
