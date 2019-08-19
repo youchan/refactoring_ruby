92c22c3f5cf27594236c683eea23cac12ed8988c
ken3ypa <kirie115@gmail.com>
2019-08-19 09:16:04 +0900

    ANDで条件を一文にまとめる

--- cp9/9_3_consolidate_conditional_expression.rb
+++ cp9/9_3_consolidate_conditional_expression.rb
@@ -1,8 +1,6 @@
 # 9.3 条件式の統合（Consolidate Conditional Expression）
 
-if on_vacation?
-  if length_of_service > 10
-    return 1
-  end
+if on_vacation? && length_of_service > 10
+  return 1
 end
 0.5
