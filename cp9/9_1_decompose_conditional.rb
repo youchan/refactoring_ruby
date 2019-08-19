f6c5143f0b62af5499cef63846396783f0fcc0f2
ken3ypa <kirie115@gmail.com>
2019-08-19 09:05:04 +0900

    条件文の分解

--- cp9/9_1_decompose_conditional.rb
+++ cp9/9_1_decompose_conditional.rb
@@ -1,1 +1,7 @@
 # 9.1 条件文の分解（Decompose Conditional）
+
+if date < SUMMER_START || date > SUMMER_END
+  charge = quantity * @winter_rate + @winter_service_charge
+else
+  charge = quantity * @summer_rate
+end
