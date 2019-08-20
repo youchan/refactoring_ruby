76f45db6c2c93401aaf14c7f9c24f6c4f824f787
ken3ypa <kirie115@gmail.com>
2019-08-20 19:44:54 +0900

    戻り値を明示して返す

--- cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
+++ cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
@@ -1,9 +1,7 @@
 # 9.6 条件分岐のネストからガード節へ（Replace Nested Conditional with Guard Clauses）
 
 def adjusted_capital
-  result = 0.0
-  return result if @capital <= 0.0
-  return result if @interest_rate <= 0.0 || @duration <= 0.0
-  result = (@income / @duration) * ADJ_FACTOR
-  result
+  return 0.0 if @capital <= 0.0
+  return 0.0 if @interest_rate <= 0.0 || @duration <= 0.0
+  (@income / @duration) * ADJ_FACTOR
 end
