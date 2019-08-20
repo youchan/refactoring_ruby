cac0ca1b4f9d0f128a19695c553a0905184d667e
ken3ypa <kirie115@gmail.com>
2019-08-20 19:38:05 +0900

    条件の反転

--- cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
+++ cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
@@ -1,8 +1,11 @@
 # 9.6 条件分岐のネストからガード節へ（Replace Nested Conditional with Guard Clauses）
 
-def pay_amount
-  return dead_amount if @dead
-  return separated_amount if @separated
-  return retired_amount if @retired
-  normal_pay_amount
+def adjusted_capital
+  result = 0.0
+  if @capital > 0.0
+    if @interest_rate > 0.0 && @duration > 0.0
+      result = (@income / @duration) * ADJ_FACTOR
+    end
+  end
+  result
 end
