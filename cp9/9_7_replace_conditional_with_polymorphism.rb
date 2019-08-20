44a7581fd2ca377c9a0392dca1a8a1df10aaf1b7
ken3ypa <kirie115@gmail.com>
2019-08-20 19:46:19 +0900

    条件分岐からポリモーフィズムへ

--- cp9/9_7_replace_conditional_with_polymorphism.rb
+++ cp9/9_7_replace_conditional_with_polymorphism.rb
@@ -1,1 +1,14 @@
 # 9.7 条件分岐からポリモーフィズムへ（Replace Conditional with Polymorphism）
+
+class MountainBike
+  def price
+    case @type_code
+    when :rigid
+      (1 + @commission) * @base_price
+    when :front_suspension
+      (1 + @commission) * @base_price + @front_suspension_price
+    when :full_suspension
+      (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
+    end
+  end
+end
