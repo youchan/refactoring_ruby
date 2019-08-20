33acb6ea7b57db279dfdc273ce4c3e34edd42a82
ken3ypa <kirie115@gmail.com>
2019-08-20 09:41:15 +0900

    制御フラグの除去 EXAMPLE

--- cp9/9_5_remove_control_flag.rb
+++ cp9/9_5_remove_control_flag.rb
@@ -1,11 +1,17 @@
 # 9.5 制御フラグの除去（Remove Control Flag）
 
-done = false
-until done do
-  if (condition)
-    # do something
-    done = true
+def check_security(people)
+  found = false
+  people.each do |person|
+    unless found
+      if person == "Don"
+        send_alert
+        found = true
+      end
+      if person == "John"
+        send_alert
+        found = true
+      end
+    end
   end
-  value -= 1
 end
-
