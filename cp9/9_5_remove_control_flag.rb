5e20a39b6b9ab0c038fcddd463542fcba4a86ffd
ken3ypa <kirie115@gmail.com>
2019-08-20 09:45:30 +0900

    Returnを使ったフラグ制御

--- cp9/9_5_remove_control_flag.rb
+++ cp9/9_5_remove_control_flag.rb
@@ -1,14 +1,19 @@
 # 9.5 制御フラグの除去（Remove Control Flag）
 
-def check_security(people)
+def example(people)
+  found = ""
   people.each do |person|
-    if person == "Don"
-      send_alert
-      break
-    end
-    if person == "John"
-      send_alert
-      break
+    if found == ""
+      if person == "Don"
+        send_alert
+        found = "Don"
+      end
+      if person == "John"
+        send_alert
+        found = "John"
+      end
     end
   end
+  some_later_code(found)
 end
+
