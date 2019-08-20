16bc5bbc3619c23e8d8da12bb87be439cf107b9a
ken3ypa <kirie115@gmail.com>
2019-08-20 09:43:41 +0900

    フラグを削除

--- cp9/9_5_remove_control_flag.rb
+++ cp9/9_5_remove_control_flag.rb
@@ -1,17 +1,14 @@
 # 9.5 制御フラグの除去（Remove Control Flag）
 
 def check_security(people)
-  found = false
   people.each do |person|
-    unless found
-      if person == "Don"
-        send_alert
-        break
-      end
-      if person == "John"
-        send_alert
-        break
-      end
+    if person == "Don"
+      send_alert
+      break
+    end
+    if person == "John"
+      send_alert
+      break
     end
   end
 end
