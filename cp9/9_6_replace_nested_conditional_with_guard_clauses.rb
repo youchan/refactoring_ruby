f9aa64e916ac1e6eebb4078e4bcae37cd31c1294
ken3ypa <kirie115@gmail.com>
2019-08-20 09:49:27 +0900

    条件分岐のネスト からガード節へ

--- cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
+++ cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
@@ -1,1 +1,18 @@
 # 9.6 条件分岐のネストからガード節へ（Replace Nested Conditional with Guard Clauses）
+
+def pay_amount
+  if @dead
+    result = dead_amount
+  else
+    if @separated
+      result = separated_amount
+    else
+      if @retired
+        result = retired_amount
+      else
+        result = normal_pay_amount
+      end
+    end
+  end
+  result
+end
