d8440661166f0beed19979c01cec61f79b3c5227
ken3ypa <kirie115@gmail.com>
2019-08-20 19:44:03 +0900

    条件を反転させてリターン

--- cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
+++ cp9/9_6_replace_nested_conditional_with_guard_clauses.rb
@@ -3,7 +3,7 @@
 def adjusted_capital
   result = 0.0
   return result if @capital <= 0.0
-  return result if !(@interest_rate > 0.0 && @duration > 0.0)
+  return result if @interest_rate <= 0.0 || @duration <= 0.0
   result = (@income / @duration) * ADJ_FACTOR
   result
 end
