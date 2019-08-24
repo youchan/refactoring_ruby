d68d808b5bc50be7e529651c8e46d134ca707360
ken3ypa <kirie115@gmail.com>
2019-08-25 08:38:38 +0900

    init

--- cp9/9_8_introduce_null_object.rb
+++ cp9/9_8_introduce_null_object.rb
@@ -1,1 +1,22 @@
 # 9.8 nullオブジェクトの導入（Introduce Null Object）
+
+class Site
+  attr_reader :customer
+end
+
+class Customer
+  attr_reader :name, :plan, :history
+end
+
+class PaymentHistory
+  def weeks_delinquent_in_last_year; end
+end
+
+class BillingPlan
+  attr_reader :basic
+end
+
+customer = Site.new.customer
+plan = customer ? customer.plan : BillingPlan.basic
+customer_name = customer ? customer.name : 'occupant'
+weeks_delinquent = customer.nil? ? 0 : customer.history.weeks_delinquent_in_last_years
--- cp9/9_9_introduce_assertion.rb
+++ cp9/9_9_introduce_assertion.rb
@@ -1,2 +1,7 @@
 # 9.9 アサーションの導入（Introduce Assertion）
 
+def expense_limit
+  (@expense_limit != NULL_EXPENSE) ? \
+    @expense_limit : \
+    @primary_project.member_expense_limit
+end
