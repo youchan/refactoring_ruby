139506cb349370934983fce93341cb130e490bea
ken3ypa <kirie115@gmail.com>
2019-08-25 09:12:08 +0900

    データ値からオブジェクトへ

---
+++ cp8/8_2_replace_data_value_with_object.rb
@@ -0,0 +1,13 @@
+class Order
+  attr_accessor :customer
+
+  def initialize(customer)
+    @customer = customer
+  end
+
+  private
+
+  def self.number_of_orders_for(orders, customer)
+    orders.select { |order| order.customer == customer }.size
+  end
+end
