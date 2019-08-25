b4315dd33693320158f0d7d2d7a5ed40ad5d43b4
ken3ypa <kirie115@gmail.com>
2019-08-25 09:07:35 +0900

    サブクラスを作成

--- cp8/8_1_self_encapsulate_field.rb
+++ cp8/8_1_self_encapsulate_field.rb
@@ -18,3 +18,17 @@ class Item
     base_price * (1 + tax_rate)
   end
 end
+
+class ImportedItem < Item
+  attr_reader :import_duty
+
+  def initialize(base_price, tax_rate, import_duty)
+    super(base_price, tax_rate)
+    @import_duty = import_duty
+  end
+
+  # NOTE: tax_rate に import_duty の値を加えて初期化している
+  def tax_rate
+    super + import_duty
+  end
+end
