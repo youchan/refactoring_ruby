ab6a3ae2f1bf9cb14bc82090476aadcfb6fd8ded
ken3ypa <kirie115@gmail.com>
2019-08-26 19:32:33 +0900

    ハッシュからオブジェクトへ

---
+++ cp8/8_6_replace_hash_with_object.rb
@@ -0,0 +1,6 @@
+new_network = { nodes: [], old_networks: [] }
+new_network[:old_networks] << node.network
+new_network[:nodes] << node
+new_network[:name] = new_network[:old_networks]
+                         .map(&:name)
+                         .join(' - ')
