2633fb9f5ecbdb1710778bd57a8524463fe1dd2c
ken3ypa <kirie115@gmail.com>
2019-08-28 08:48:57 +0900

    範囲を引数で渡すようにして分岐させる

--- cp10/10_5_parameterize_method.rb
+++ cp10/10_5_parameterize_method.rb
@@ -1,12 +1,16 @@
 def base_charge
-  result = [last_usage, 100].min * 0.03
-  if last_usage > 100
-    result += ([last_usage, 200].min - 100) * 0.05
-  end
-  if last_usage > 200
-    result += (last_usage - 200) * 0.07
-  end
+  result =  (usage_in_range 0..100) * 0.03
+  result += (usage_in_range 100..200) * 0.05
+  result += (usage_in_range 200..last_usage) * 0.07
   Dollar.new(result)
+end
+
+def usage_in_range(range)
+  if last_usage > range.begin
+    [last_usage, range.end].min - range.begin
+  else
+    0
+  end
 end
 
 def last_usage
