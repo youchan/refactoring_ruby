6fdb8e5dc4ade6795c3f8a0d44ecbc71ccf9b5f4
ken3ypa <kirie115@gmail.com>
2019-08-28 08:46:31 +0900

    シンプルに呼び出せる別の例

--- cp10/10_5_parameterize_method.rb
+++ cp10/10_5_parameterize_method.rb
@@ -1,5 +1,13 @@
-class Employee
-  def raise(factor)
-    @salary *= (1 + factor)
+def base_charge
+  result = [last_usage, 100].min * 0.03
+  if last_usage > 100
+    result += ([last_usage, 200].min - 100) * 0.05
   end
+  if last_usage > 200
+    result += (last_usage - 200) * 0.07
+  end
+  Dollar.new(result)
+end
+
+def last_usage
 end
