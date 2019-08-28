91f629d66943bec96aa6746d1c5f354cb5a4b9af
ken3ypa <kirie115@gmail.com>
2019-08-28 09:24:08 +0900

    引数を渡す

--- cp10/10_7_preserve_whole_object.rb
+++ cp10/10_7_preserve_whole_object.rb
@@ -2,12 +2,12 @@ class Room
   def within_plan?(plan)
     low = days_temperature_range.low
     high = days_temperature_range.high
-    plan.within_range?(low, high)
+    plan.within_range?(days_temperature_range, low, high)
   end
 end
 
 class HeatingPlan
-  def within_range?(low, high)
+  def within_range?(room_range, low, high)
     (low >= @range.low) && (high <= @range.high)
   end
 end
