01113ac14ed5c19120f8f49b163ba7a13d499c72
ken3ypa <kirie115@gmail.com>
2019-08-28 09:25:56 +0900

    渡した引数で値をとるようlowを書き換える

--- cp10/10_7_preserve_whole_object.rb
+++ cp10/10_7_preserve_whole_object.rb
@@ -2,12 +2,12 @@ class Room
   def within_plan?(plan)
     low = days_temperature_range.low
     high = days_temperature_range.high
-    plan.within_range?(days_temperature_range, low, high)
+    plan.within_range?(days_temperature_range, high)
   end
 end
 
 class HeatingPlan
-  def within_range?(room_range, low, high)
-    (low >= @range.low) && (high <= @range.high)
+  def within_range?(room_range, high)
+    (room_range.low >= @range.low) && (high <= @range.high)
   end
 end
