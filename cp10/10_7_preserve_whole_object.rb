2e57172f80a3f419a939876da7854133e947dc30
ken3ypa <kirie115@gmail.com>
2019-08-28 09:22:07 +0900

    RoomオブジェクトとHeatingPlanオブジェクトを追加
    1日の最高/最低気温を記録するRoomと、それを元にプラン内かを判断するHeatingPlan

--- cp10/10_7_preserve_whole_object.rb
+++ cp10/10_7_preserve_whole_object.rb
@@ -1,1 +1,13 @@
-plan.within_range?(days_temperature_range)
+class Room
+  def within_plan?(plan)
+    low = days_temperature_range.low
+    high = days_temperature_range.high
+    plan.within_range?(low, high)
+  end
+end
+
+class HeatingPlan
+  def within_range?(low, high)
+    (low >= @range.low) && (high <= @range.high)
+  end
+end
