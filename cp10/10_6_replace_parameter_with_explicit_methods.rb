043f2bed14340f44507cae3308d1fbf299db3c88
ken3ypa <kirie115@gmail.com>
2019-08-28 09:18:23 +0900

    処理をEmployeeクラスに所属させる

--- cp10/10_6_replace_parameter_with_explicit_methods.rb
+++ cp10/10_6_replace_parameter_with_explicit_methods.rb
@@ -1,29 +1,30 @@
-ENGINEER = 0
-SALESPERSON = 1
-MANAGER = 2
+class Employee
+  ENGINEER = 0
+  SALESPERSON = 1
+  MANAGER = 2
 
-def self.create(type)
-  case type
-  when ENGINEER
-    Engineer.create_engineer
-  when SALESPERSON
-    Salesperson.create_salesperson
-  when MANAGER
-    Manager.create_manager
-  else
-    raise ArgumentError, "Incorrect type code value"
+  def self.create(type)
+    case type
+    when ENGINEER
+      Engineer.create_engineer
+    when SALESPERSON
+      Salesperson.create_salesperson
+    when MANAGER
+      Manager.create_manager
+    else
+      raise ArgumentError, "Incorrect type code value"
+    end
+  end
+
+  def self.create_engineer
+    Engineer.new
+  end
+
+  def self.create_salesperson
+    Salesperson.new
+  end
+
+  def self.create_manager
+    Manager.new
   end
 end
-
-def self.create_engineer
-  Engineer.new
-end
-
-def self.create_salesperson
-  Salesperson.new
-end
-
-def self.create_manager
-  Manager.new
-end
-
