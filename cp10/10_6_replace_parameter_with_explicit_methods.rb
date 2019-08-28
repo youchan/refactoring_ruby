1b599738c955b3c39d2bd83669870ade2dd84075
ken3ypa <kirie115@gmail.com>
2019-08-28 09:16:12 +0900

    明示的に別々のメソッドを作る

--- cp10/10_6_replace_parameter_with_explicit_methods.rb
+++ cp10/10_6_replace_parameter_with_explicit_methods.rb
@@ -14,3 +14,14 @@ def self.create(type)
     raise ArgumentError, "Incorrect type code value"
   end
 end
+
+def self.create_engineer
+  Engineer.new
+end
+def self.create_salesperson
+  Salesperson.new
+end
+def self.create_manager
+  Manager.new
+end
+
