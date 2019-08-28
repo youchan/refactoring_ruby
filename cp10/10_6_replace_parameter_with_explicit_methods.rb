726ae391c0486c1d1c569fd4252941b94ed27037
ken3ypa <kirie115@gmail.com>
2019-08-28 09:15:00 +0900

    渡された引数に基づいて Employee のサブクラスを作る例

--- cp10/10_6_replace_parameter_with_explicit_methods.rb
+++ cp10/10_6_replace_parameter_with_explicit_methods.rb
@@ -1,7 +1,16 @@
-def height=(value)
-  @height = value
-end
+ENGINEER = 0
+SALESPERSON = 1
+MANAGER = 2
 
-def width=(value)
-  @width = value
+def self.create(type)
+  case type
+  when ENGINEER
+    Engineer.new
+  when SALESPERSON
+    Salesperson.new
+  when MANAGER
+    Manager.new
+  else
+    raise ArgumentError, "Incorrect type code value"
+  end
 end
