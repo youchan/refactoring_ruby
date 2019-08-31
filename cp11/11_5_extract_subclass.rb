d7a5e021ee392c31744d63c9349e81072cad3a88
ken3ypa <kirie115@gmail.com>
2019-08-31 15:52:53 +0900

    init

---
+++ cp11/11_5_extract_subclass.rb
@@ -0,0 +1,30 @@
+class JobItem
+  attr_reader :quantity, :employee
+
+  def initialize(unit_price, quantity, is_labor, employee)
+    @unit_price = unit_price
+    @quantity = quantity
+    @is_labor = is_labor
+    @employee = employee
+  end
+
+  def total_price
+    unit_price * @quantity
+  end
+
+  def unit_price
+    labor? ? @employee.rate : @unit_price
+  end
+
+  def labor?
+    @is_labor
+  end
+end
+
+class Employee
+  attr_reader :rate
+
+  def initialize(rate)
+    @rate = rate
+  end
+end
