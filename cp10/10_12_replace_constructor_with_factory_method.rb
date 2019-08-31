75e2e491824a4bda0f5a0ee5669ae9cfd387446e
ken3ypa <kirie115@gmail.com>
2019-08-31 13:16:11 +0900

    製品を作るサンプル。作りたい製品のタイプは基本価格と輸入品か否かによって決まる
    製品タイプを表すために継承階層も作ってある

--- cp10/10_12_replace_constructor_with_factory_method.rb
+++ cp10/10_12_replace_constructor_with_factory_method.rb
@@ -1,19 +1,35 @@
 class ProductController
   def create
-    @product = Product.create(base_price, imported)
+    @product = if imported
+                 ImportedProduct.new(base_price)
+               else
+                 if base_price > 1000
+                   LuxuryProduct.new(base_price)
+                 else
+                   Product.new(base_price)
+                 end
+               end
   end
 end
 
 class Product
-  def self.create(base_price, imported = false)
-    if imported
-      ImportedProduct.new(base_price)
-    else
-      if base_price > 1000
-        LuxuryProduct.new(base_price)
-      else
-        Product.new(base_price)
-      end
-    end
+  def initialize(base_price)
+    @base_price = base_price
+  end
+
+  def total_price
+    @base_price
+  end
+end
+
+class LuxuryProduct < Product
+  def total_price
+    super + 0.1 * super
+  end
+end
+
+class ImportedProduct < Product
+  def total_price
+    super + 0.25 * super
   end
 end
