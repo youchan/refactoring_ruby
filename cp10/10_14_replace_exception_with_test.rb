5c8fbdb87be8fff96de0802a3e9688f925392fa5
ken3ypa <kirie115@gmail.com>
2019-08-31 15:37:07 +0900

    EXAMPLE

--- cp10/10_14_replace_exception_with_test.rb
+++ cp10/10_14_replace_exception_with_test.rb
@@ -1,4 +1,24 @@
-def execute(command)
-  command.prepare if command.respond_to? :prepare
-  command.execute
+class ResourceStack
+  def pop
+    #raises EmptyStackError if the stack is empty
+  end
+end
+
+class ResourcePool
+  def initialize
+    @available = ResourceStack.new
+    @allocated = ResourceStack.new
+  end
+
+  def resource
+    begin
+      result = @available.pop
+      @allocated.push(result)
+      return result
+    rescue EmptyStackError
+      result = Resource.new
+      @allocated.push(result)
+      return result
+    end
+  end
 end
