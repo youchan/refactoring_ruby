e39a56387655f2206e5a941fbb5fcbf08ea3600d
ken3ypa <kirie115@gmail.com>
2019-08-31 12:40:11 +0900

    discounted_priceをインライン化する

--- cp10/10_8_replace_parameter_with_method.rb
+++ cp10/10_8_replace_parameter_with_method.rb
@@ -1,14 +1,10 @@
 def price
-  discounted_price
+  return base_price * 0.1 if discount_level == 2
+  base_price * 0.05
 end
 
 def base_price
   @quantity * @item_price
-end
-
-def discounted_price
-  return base_price * 0.1 if discount_level == 2
-  base_price * 0.05
 end
 
 def discount_level
