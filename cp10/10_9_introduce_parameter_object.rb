4237626955fd7f494cda2ad685eff58bfad9b817
ken3ypa <kirie115@gmail.com>
2019-08-31 12:49:14 +0900

    Charge (代金)オブジェクトに基本価格、税率、輸入品をまとめる

--- cp10/10_9_introduce_parameter_object.rb
+++ cp10/10_9_introduce_parameter_object.rb
@@ -10,6 +10,16 @@ class Account
   end
 end
 
+class Charge
+  attr_accessor :base_price, :tax_rate, :imported
+
+  def initialize(base_price, tax_rate, imported)
+    @base_price = base_price
+    @tax_rate = tax_rate
+    @imported = imported
+  end
+end
+
 account.add_charge(5, 0.1, true)
 account.add_charge(12, 0.125, false)
 total = account.total_charge
