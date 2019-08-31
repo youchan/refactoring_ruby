ee20804aa3b0323156589ff82154262ca5d6627a
ken3ypa <kirie115@gmail.com>
2019-08-31 15:17:59 +0900

    Assertionを導入し、より厳格にエラーを出すように設定する

--- cp10/10_13_replace_error_code_with_exception.rb
+++ cp10/10_13_replace_error_code_with_exception.rb
@@ -1,10 +1,29 @@
+class Account
+  include Assertions
+
+  def withdraw(amount)
+    assert("amonunt too large") { amount > @balance }
+    @balance -= amount
+  end
+
+  def can_withdraw?(amount)
+    true
+  end
+end
+
+module Assertions
+  class AssertionFailedError < StandardError; end
+  class BalanceError < Standarderror; end
+
+  def assert(message, &condition)
+    unless condition.call
+      raise AssertionFailedError.new("Assertion Failed: #{message}")
+    end
+  end
+end
+
 if !account.can_withdraw?(amount)
   handle_overdrawn
 else
   account.withdraw(amount)
-end
-
-def withdraw(amount)
-  raise ArgumentError.new if amount > @balance
-  @balance -= amount
 end
