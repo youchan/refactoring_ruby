ba4e00ca79e11ea52a2fb75a26a9ce6216d95266
ken3ypa <kirie115@gmail.com>
2019-08-31 12:34:44 +0900

    メソッドを抽出する

--- cp10/10_8_replace_parameter_with_method.rb
+++ cp10/10_8_replace_parameter_with_method.rb
@@ -1,2 +1,15 @@
-base_price = @quantity * @item_price
-final_price = discounted_price(base_price)
+def price
+  base_price = @quantity * @item_price
+  level_of_discount = discount_level
+  discounted_price(base_price, level_of_discount)
+end
+
+def discounted_price(base_price, level_of_discount)
+  return base_price * 0.1 if level_of_discount == 2
+  base_price * 0.05
+end
+
+def discount_level
+  return 2 if @quantity > 100
+  return 1
+end
