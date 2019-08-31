ec85ceec77c25695fabd2af940faf40f7c376c5b
ken3ypa <kirie115@gmail.com>
2019-08-31 13:13:38 +0900

    コンストラクタを取り除いて、ファクトリメソッドを作る
    オブジェクトを作成するときに、単なる構築以上をしたい場合

--- cp10/10_12_replace_constructor_with_factory_method.rb
+++ cp10/10_12_replace_constructor_with_factory_method.rb
@@ -1,13 +1,19 @@
 class ProductController
   def create
-    @product = if imported
-                 ImportedProduct.new(base_price)
-               else
-                 if base_price > 1000
-                   LuxuryProduct.new(base_price)
-                 else
-                   Product.new(base_price)
-                 end
-               end
+    @product = Product.create(base_price, imported)
+  end
+end
+
+class Product
+  def self.create(base_price, imported = false)
+    if imported
+      ImportedProduct.new(base_price)
+    else
+      if base_price > 1000
+        LuxuryProduct.new(base_price)
+      else
+        Product.new(base_price)
+      end
+    end
   end
 end
