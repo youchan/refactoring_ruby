33956128fc5f648950f4836e76c3672edbd36b7c
ken3ypa <kirie115@gmail.com>
2019-08-31 13:18:37 +0900

    クラスメソッドとしてロジックを抽出する

--- cp10/10_12_replace_constructor_with_factory_method.rb
+++ cp10/10_12_replace_constructor_with_factory_method.rb
@@ -1,14 +1,18 @@
 class ProductController
   def create
-    @product = if imported
-                 ImportedProduct.new(base_price)
-               else
-                 if base_price > 1000
-                   LuxuryProduct.new(base_price)
-                 else
-                   Product.new(base_price)
-                 end
-               end
+    @product = self.class.create_product(base_price, imported)
+  end
+
+  def self.create_product(base_price, imported)
+    if imported
+      ImportedProduct.new(base_price)
+    else
+      if base_price > 1000
+        LuxuryProduct.new(base_price)
+      else
+        Product.new(base_price)
+      end
+    end
   end
 end
 
