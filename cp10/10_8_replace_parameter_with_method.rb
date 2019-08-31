72a9102542fffc064e6076d8e8ee36484f9de9ed
ken3ypa <kirie115@gmail.com>
2019-08-31 12:39:06 +0900

    値の引数と一時変数を取り除く

--- cp10/10_8_replace_parameter_with_method.rb
+++ cp10/10_8_replace_parameter_with_method.rb
@@ -1,9 +1,12 @@
 def price
-  base_price = @quantity * @item_price
-  discounted_price(base_price)
+  discounted_price
 end
 
-def discounted_price(base_price)
+def base_price
+  @quantity * @item_price
+end
+
+def discounted_price
   return base_price * 0.1 if discount_level == 2
   base_price * 0.05
 end
