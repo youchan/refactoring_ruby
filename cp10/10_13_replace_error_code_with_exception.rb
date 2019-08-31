7111c925f82bdee3a834cbebb053f616c9571d40
ken3ypa <kirie115@gmail.com>
2019-08-31 15:22:25 +0900

    呼び出し元がたくさんあるケース

--- cp10/10_13_replace_error_code_with_exception.rb
+++ cp10/10_13_replace_error_code_with_exception.rb
@@ -1,9 +1,14 @@
-class Account
-  include Assertions
+if account.withdraw(amount) == -1
+  handle_overdrawn
+else
+  do_the_usual_thing
+end
 
+class Account
   def withdraw(amount)
-    raise BalanceError.new if amount > @balance
+    return -1 if amount > @balance
     @balance -= amount
+    return 0
   end
 
   def can_withdraw?(amount)
@@ -20,11 +25,4 @@ module Assertions
       raise AssertionFailedError.new("Assertion Failed: #{message}")
     end
   end
-end
-
-begin
-  account.withdraw(amount)
-  do_the_usual_thing
-rescue BalanceError
-  handle_overdrawn
 end
