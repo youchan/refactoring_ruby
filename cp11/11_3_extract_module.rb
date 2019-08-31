f50cc8faedda0b9b9781e25e160807528e44e2a5
ken3ypa <kirie115@gmail.com>
2019-08-31 15:48:09 +0900

    before_save の処理をmoduleに切り出す

--- cp11/11_3_extract_module.rb
+++ cp11/11_3_extract_module.rb
@@ -1,5 +1,13 @@
 class Bid
-  before_save :capture_account_number
+  include AccountNumberCapture
+end
+
+module AccountNumberCapture
+  def self.included(klass)
+    klass.class_eval do
+      before_save :capture_account_number
+    end
+  end
 
   def capture_account_number
     self.account_number = buyer.preferred_account_number
