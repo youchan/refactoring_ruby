03b19c546eac254ed84ddbd47f52c17b5ca77021
ken3ypa <kirie115@gmail.com>
2019-09-02 19:30:39 +0900

    重複部分の削除

--- cp11/11_6_introduce_inheritance.rb
+++ cp11/11_6_introduce_inheritance.rb
@@ -12,16 +12,11 @@ class MountainBike
 end
 
 class FrontSuspensionMountainBike < MountainBike
-  TIRE_WIDTH_FACTOR = 6
-  FRONT_SUSPENSION_FACTOR = 8
-  attr_accessor :tire_diameter, :front_fork_travel
 
-  def wheel_circumference
-    Math::PI * (@wheel_diameter + @tire_diameter)
-  end
+  FRONT_SUSPENSION_FACTOR = 8
+  attr_accessor :front_fork_travel
 
   def off_road_ability
-    @tire_diameter * TIRE_WIDTH_FACTOR + @front_fork_travel *
-        FRONT_SUSPENSION_FACTOR
+    super + @front_fork_travel * FRONT_SUSPENSION_FACTOR
   end
 end
