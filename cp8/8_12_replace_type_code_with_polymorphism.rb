b30a48742be60b0044f1319577a1d1d3f84081d1
ken3ypa <kirie115@gmail.com>
2019-09-03 09:49:17 +0900

    init

---
+++ cp8/8_12_replace_type_code_with_polymorphism.rb
@@ -0,0 +1,33 @@
+class MountainBike
+  def initialize(params)
+    params.each {|key, value| instance_variable_set "@#{key}", value}
+  end
+
+  def off_road_ability
+    result = @tire_width * TIRE_WIDTH_FACTOR
+    if @type_code == :front_suspension || @type_code == :full_suspension
+      result += @front_fork_travel * FRONT_SUSPENSION_FACTOR
+    end
+    if @type_code == :full_suspension
+      result += @rear_fork_travel * REAR_SUSPENSION_FACTOR
+    end
+    result
+  end
+
+  def price
+    case @type_code
+    when :rigid
+      (1 + @commission) * @base_price
+    when :front_suspension
+      (1 + @commission) * @base_price + @front_suspension_price
+    when :full_suspension
+      (1 + @commission) * @base_price + @front_suspension_price +
+          @rear_suspension_price
+    end
+  end
+end
+
+
+bike = MountainBike.new(:type_code => :rigid, :tire_width => 2.5)
+bike2 = MountainBike.new(:type_code => :front_suspension, :tire_width => 2,
+                         :front_fork_travel => 3)
