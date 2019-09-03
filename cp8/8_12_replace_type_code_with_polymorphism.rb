78e5898a9aa66c9e578369497771cd60347d135f
ken3ypa <kirie115@gmail.com>
2019-09-03 09:58:05 +0900

    off_road_abilityメソッドのcase文の分岐条件をそれぞれのクラスに定義し、元のメソッドを削除

--- cp8/8_12_replace_type_code_with_polymorphism.rb
+++ cp8/8_12_replace_type_code_with_polymorphism.rb
@@ -3,17 +3,6 @@ module MountainBike
   def wheel_circumference
     Math::PI * (@wheel_diameter + @tire_diameter)
   end
-
-  def off_road_ability
-    result = @tire_width * TIRE_WIDTH_FACTOR
-    if @type_code == :front_suspension || @type_code == :full_suspension
-      result += @front_fork_travel * FRONT_SUSPENSION_FACTOR
-    end
-    if @type_code == :full_suspension
-      result += @rear_fork_travel * REAR_SUSPENSION_FACTOR
-    end
-    result
-  end
 end
 
 class RigidMountainBike
@@ -22,6 +11,11 @@ class RigidMountainBike
   def price
     (1 + @commission) * @base_price
   end
+
+
+  def off_road_ability
+    @tire_width * TIRE_WIDTH_FACTOR
+  end
 end
 class FrontSuspensionMountainBike
   include MountainBike
@@ -29,12 +23,22 @@ class FrontSuspensionMountainBike
   def price
     (1 + @commission) * @base_price + @front_suspension_price
   end
+
+  def off_road_ability
+    @tire_width * TIRE_WIDTH_FACTOR + @front_fork_travel *
+        FRONT_SUSPENSION_FACTOR
+  end
 end
 class FullSuspensionMountainBike
   include MountainBike
 
   def price
     (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
+  end
+
+  def off_road_ability
+    @tire_width * TIRE_WIDTH_FACTOR + @front_fork_travel *
+        FRONT_SUSPENSION_FACTOR + @rear_fork_travel * REAR_SUSPENSION_FACTOR
   end
 end
 
