4f28f2bc35f3669ba87ae948588a060fefa426fd
ken3ypa <kirie115@gmail.com>
2019-09-03 09:08:36 +0900

    両方のポインタを同期させるためのメソッドを追加する

--- cp8/8_7_change_unidirectional_association_to_bidirectional.rb
+++ cp8/8_7_change_unidirectional_association_to_bidirectional.rb
@@ -1,3 +1,12 @@
 class Order
   attr_accessor :customer
 end
+
+require 'set'
+
+class Customer
+  def friend_orders
+    #should only be used by Order when modifying the association
+    @orders
+  end
+end
