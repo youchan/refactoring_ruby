7e6e22ef6792cca310c3b784e66df38b78076335
簗瀬健介 <kirie115@gmail.com>
2019-09-15 08:58:37 +0900

    [SAMPLE2]メソッドの抽出を使って書き換える例

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -1,9 +1,8 @@
 # 6.6 説明用変数の導入(EXAMPLE)
 
 def price
-  base_price = @quantity * @item_price
-  quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
-  shipping = [base_price * 0.1, 100.0].min
-
-  base_price - quantity_discount + shipping
+  # price is base price - quantity discount + shipping
+  return @quantity * @item_price -
+      [0, @quantity - 500].max * @item_price * 0.05 +
+      [@quantity * @item_price * 0.1, 100.0].min
 end
