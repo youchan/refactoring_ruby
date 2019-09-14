d29bb0327ab4028a2fa61b8ed4b590bb4dab8bae
簗瀬健介 <kirie115@gmail.com>
2019-09-15 08:55:15 +0900

    不要になったコメントを削除する

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -1,7 +1,6 @@
 # 6.6 説明用変数の導入(EXAMPLE)
 
 def price
-  # price is base price - quantity discount + shipping
   base_price = @quantity * @item_price
   quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
   shipping = [base_price * 0.1, 100.0].min
