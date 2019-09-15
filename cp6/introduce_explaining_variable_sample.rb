ff6b7369f484a5d4a23a64ecceabc1cea12cf3f4
簗瀬健介 <kirie115@gmail.com>
2019-09-15 09:01:15 +0900

    [SAMPLE2]base_priceをメソッドにする

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -2,7 +2,10 @@
 
 def price
   # price is base price - quantity discount + shipping
-  return @quantity * @item_price -
-      [0, @quantity - 500].max * @item_price * 0.05 +
-      [@quantity * @item_price * 0.1, 100.0].min
+  return base_price -
+      [0, @quantity - 500].max * @item_price * 0.05 + [base_price * 0.1, 100.0].min
+end
+
+def base_price
+  @quantity * @item_price
 end
