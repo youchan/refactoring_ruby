6495a8e9358a620f2784170c8fd9ae69084187ef
簗瀬健介 <kirie115@gmail.com>
2019-09-15 09:02:25 +0900

    [SAMPLE2]他の計算式もメソッドに抽出し、書き換える

--- cp6/introduce_explaining_variable_sample.rb
+++ cp6/introduce_explaining_variable_sample.rb
@@ -1,11 +1,17 @@
 # 6.6 説明用変数の導入(EXAMPLE)
 
 def price
-  # price is base price - quantity discount + shipping
-  return base_price -
-      [0, @quantity - 500].max * @item_price * 0.05 + [base_price * 0.1, 100.0].min
+  base_price - quantity_discount + shipping
 end
 
 def base_price
   @quantity * @item_price
+end
+
+def quantity_discount
+  [0, @quantity - 500].max * @item_price * 0.05
+end
+
+def shipping
+  [base_price * 0.1, 100.0].min
 end
